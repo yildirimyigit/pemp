6ca964ca9ac2bb12eec7bbed74f907d560c437a0
Yigit Yildirim <yigit.yildirim@boun.edu.tr>
2025-01-02 10:11:08 +0300

    minor

--- sim/adroit_test.py
+++ sim/adroit_test.py
@@ -11,7 +11,7 @@ from stable_baselines3.common.evaluation import evaluate_policy
 gym.register_envs(gymnasium_robotics)
 
 env = gym.make('AdroitHandHammer-v1', render_mode='human')
-env.reset()
+o,i = env.reset()
 
 model = PPO.load("ppo_adroithand_hammer_", env=env, device='cpu')
 
@@ -19,14 +19,15 @@ obs, _ = env.reset()
 for i in range(10000):
     action, _states = model.predict(obs, deterministic=True)
     obs, rewards, term, trunc, info = env.step(action)
-    print(obs[42:45])
+    print(obs[26])
+    # print(obs[42:45])
     time.sleep(0.05)
     # if i == 23:
     #     print(env.env.env.env.get_env_state())
 
     if term or trunc:
         env.reset()
-        print('\n')
+        print('*********************','\n')
 
 time.sleep(1)
 
