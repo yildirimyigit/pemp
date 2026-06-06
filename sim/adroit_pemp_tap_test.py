68b8b04ca4a99c7e4e643ce2a8de8164885be643
Yigit Yildirim <yigit.yildirim@bogazici.edu.tr>
2026-06-06 23:01:09 +0200

    hardcoded values updated dynamically

--- sim/adroit_pemp_tap_test.py
+++ sim/adroit_pemp_tap_test.py
@@ -23,8 +23,12 @@ from positional_encoders import *
 
 env = gym.make('AdroitHandHammer-vPEMPTap', render_mode='human')
 
-out_folder = '/home/yigit/projects/pemp/outputs/sim/adroit/bare_pe/1779355082/'
+out_folder = '/home/yigit/projects/pemp/outputs/sim/adroit/bare_pe/1780764441_nail_s1/'
 model_path = out_folder + 'saved_models/'
+
+# load y_test and g_test
+y_test = torch.load(out_folder + 'y_test.pt', map_location=torch.device("cpu"))
+g_test = torch.load(out_folder + 'g_test.pt', map_location=torch.device("cpu"))
 
 # load out_folder/hyperparameters.yaml
 with open(out_folder + 'hyperparameters.yaml', 'r') as f:
@@ -33,12 +37,15 @@ with open(out_folder + 'hyperparameters.yaml', 'r') as f:
 device = 'cpu'
 t_steps = hyperparameters['t_steps']  # max length of demos
 batch_size, n_max, m_max = 1, 1, t_steps
-dx, dy, dg, dph, dpe = 1, 26, 1, 0, 27
+dx, dy, dg, dph, dpe = 1, y_test.shape[-1], g_test.shape[-1], 0, 27
 model = CNMP(input_dim=dpe+dg, output_dim=dy, n_max=n_max, m_max=m_max, encoder_hidden_dims=hyperparameters['enc_dims'], decoder_hidden_dims=hyperparameters['dec_dims'], batch_size=1, device=device)
 model.load_state_dict(torch.load(model_path + 'pe.pt', map_location='cpu', weights_only=False))
 
-pe = generate_positional_encoding(t_steps, dpe)
-g = 0.5 #hyperparameters['min_freq']
+pe = generate_positional_encoding(t_steps, dpe, hyperparameters['frequency_scaler']).to(device)
+
+test_index = 3
+
+g = g_test[test_index]  # shape (dg,)
 
 obs = torch.zeros((batch_size, n_max, dpe+dg+dy), dtype=torch.float32, device=device)
 tar_x = torch.zeros((batch_size, m_max, dpe+dg), dtype=torch.float32, device=device)
@@ -61,17 +68,7 @@ with torch.no_grad():
 
     obs[0, 0, :dpe] = pe[t]
     obs[0, 0, dpe:dpe+dg] = g  # constant
-    obs[0, 0, dpe+dg:] = torch.tensor([-0.0185, -0.6967,  0.5851, -0.8886, -1.0000,  1.0000,  1.0000,  1.0000,
-         1.0000,  1.0000,  1.0000,  1.0000, -1.0000,  1.0000,  1.0000,  1.0000,
-        -0.3020, -0.8480,  1.0000,  1.0000,  1.0000,  0.6940, -0.0790,  1.0000,
-         1.0000,  1.0000])
-    
-    # obs[0, 1, :dpe] = pe[-1]
-    # obs[0, 1, dpe:dpe+dg] = g  # constant
-    # obs[0, 1, dpe+dg:] = torch.tensor([1.0000, -1.0000, -1.0000, -1.0000, -0.6201, -1.0000, -0.5978, -1.0000,
-    #     -1.0000,  1.0000,  1.0000,  1.0000, -0.3958,  1.0000,  0.2057,  1.0000,
-    #     -1.0000, -1.0000,  0.8898,  1.0000,  1.0000,  1.0000,  1.0000,  1.0000,
-    #      0.5071,  0.0087])
+    obs[0, 0, dpe+dg:] = torch.tensor(y_test[test_index, t])  # initial observation
     
     tar_x[0, :, dpe:] = g  # constant
     tar_x[0, :, :dpe] = pe
